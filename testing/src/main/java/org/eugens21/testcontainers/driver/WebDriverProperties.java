4af39d1318e53c77c4a5f8e595073f675c427b71
Savca Eugen <savcaeugen@gmail.com>
2022-12-15 11:41:20 +0200

    * Added debug property to use for testing; * Fixed unused imports and unnecessary code; * Fixed dependencies pom.

--- testing/src/main/java/org/eugens21/testcontainers/driver/WebDriverProperties.java
+++
@@ -1,9 +0,0 @@
-package org.eugens21.testcontainers.driver;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ConfigurationProperties(prefix = "driver")
-public class WebDriverProperties {
-}
